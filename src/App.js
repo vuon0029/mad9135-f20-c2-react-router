08c906c4bba639d89c796e9afb5008c330c6191f
Vuong Tuan Kiet <55266028+vuon0029@users.noreply.github.com>
2020-10-18 23:34:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch, } from 'react-router-dom'
 import './App.css';
+import UserList from './UserList'
+import PostList from './PostList'
+import CmtList from './CmtList'
+import Info from './Info'
+import PostsFromUser from './PostsFromUser'
+import CmtsFromUser from './CmtsFromUser'
+
 
 function App() {
+  const [users, setUsers] = useState()
+  const [posts, setPosts] = useState()
+  const [comments, setComments] = useState()
+
+  useEffect(()=>{
+    fetchIt("users").then(res=>setUsers(res))
+    fetchIt("posts").then(res=>setPosts(res))
+    fetchIt("comments").then(res=>setComments(res))
+  },[])
+
+  async function fetchIt (term) {
+    const URL = `https://jsonplaceholder.typicode.com/${term}`
+
+    try {
+      const response = await fetch(URL)
+      if (!response.ok) throw new Error(response.statusText)
+      return response.json()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <NavLink to="/">Users</NavLink>
+          <NavLink to="/posts">Posts</NavLink>
+          <NavLink to="/comments">Comments</NavLink>
+        </nav>
+
+        <main>
+          <Switch>
+
+            <Route exact path="/:id/info">
+              <h2>User Info.</h2>
+              <Info users={users}/>
+            </Route>
+
+            <Route exact path="/">
+              <h2>Users</h2>
+              <UserList users={users}/>
+            </Route>
+
+            <Route exact path="/posts">
+              <h2>Posts</h2>
+              <PostList posts={posts}/>
+            </Route>
+
+            <Route exact path="/posts/:id">
+              <h2>Posts</h2>
+              <PostsFromUser posts={posts} />
+            </Route>
+
+            <Route exact path="/comments">
+              <h2>Comments</h2>
+              <CmtList comments={comments} />
+            </Route>
+
+            <Route exact path="/post/:id/comments">
+              <h2>Comments</h2>
+              <CmtsFromUser comments={comments} />
+            </Route>
+            
+          </Switch>
+
+        </main>
+      </div>
+      </Router>
+
+
   );
 }
 
